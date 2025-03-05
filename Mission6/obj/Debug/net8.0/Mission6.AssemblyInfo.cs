2a48e56ed921f1a2516d5dbc14e7a5d3a7dc5c6f
Coleman Sandy <sandcole@byu.edu>
2025-03-05 10:30:42 -0700

    finaly commit

--- Mission6/obj/Debug/net8.0/Mission6.AssemblyInfo.cs
+++ Mission6/obj/Debug/net8.0/Mission6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aefe3658dcfce6813dc21417e156046ffc5d3ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26857c1de9717690dd1c48ef2b2d4f0ff1ca4e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
